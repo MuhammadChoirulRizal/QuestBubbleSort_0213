648305f43666196bb6dc527742664aaf1a8924a4
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-03-22 23:51:50 +0700

    woyy

--- xxxxx_000.cpp
+++ xxxxx_000.cpp
@@ -2,3 +2,34 @@
 using namespace std;
 int arr[20];
 int n;
+void input()
+{
+    while (true)
+    {
+
+        cout << " Input the Maximum Length :";
+
+        cin >> n;
+
+        if (n
+     <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << " n is bigger than 20";
+        }
+    }
+    cout << endl;
+    cout << "========================" << endl;
+    cout << "enter elements of array" << endl;
+    cout << "========================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data : " << i + 1 << " : ";
+        cin >> arr[i];
+        cout << endl;
+    }
+}

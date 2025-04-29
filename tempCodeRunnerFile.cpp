89217077dd4997ecb36b2e30e23a38571800b2a7
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-04-29 10:58:08 +0700

    BinarySearch_0213

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+void input()
+{
+    while (true)
+    {
+
+        cout << " Input the Maximum Length :";
+
+        cin >> n;
+
+        if (n
+     <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << " n is bigger than 20";
+        }
+    }
+    cout << endl;
+    cout << "========================" << endl;
+    cout << "enter elements of array" << endl;
+    cout << "========================" << endl;
